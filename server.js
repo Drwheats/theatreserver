71271d130116ca5c1f4faa93c795d15b29cc72bc
Drwheats <63183755+Drwheats@users.noreply.github.com>
2026-02-04 11:40:10 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "ok",
+    message: "Hello from Oracle Cloud",
+    time: new Date().toISOString()
+  });
+});
+
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Listening on port 3000");
+});
