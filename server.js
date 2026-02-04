b8f5a9190037dcd743053675c7fc8f1d04717516
Ben <dr.wheats@gmail.com>
2026-02-04 13:58:23 -0500

    2nd api added

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/calendar", (req, res) => {
+  const filePath = path.join(__dirname, "testpayload.json");
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Failed to load payload" });
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+
+
 app.listen(3000, "0.0.0.0", () => {
   console.log("Listening on port 3000");
 });
