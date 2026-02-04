a411984ef69a0209e136900c07f4a62784e859b1
Drwheats <63183755+Drwheats@users.noreply.github.com>
2026-02-04 12:14:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.json({
     status: "ok",
-    message: "Bello from Oracle Cloud",
+    message: "Chello from Oracle Cloud",
     time: new Date().toISOString()
   });
 });
